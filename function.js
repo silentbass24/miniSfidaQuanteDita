a704effb34d2e38113ed75fb9d247ad9ee818a47
silentbass24 <silentbass24@libero.it>
2021-05-02 08:56:41 +0200

    Versione finale
    Signed-off-by: silentbass24 <silentbass24@libero.it>

--- function.js
+++ function.js
@@ -1,26 +1,25 @@
-//scriviamo una fuzione con cui ottenere un valore random tra 1 e 5
-//nella fuzione seguente otteniamo un numero tra due valori specificati
-//che non potrà mai essere più piccolo di min, nè mai uguale o superiore a max
+//function to obtain a random number from 1 to 5
+//in this way the result of function i s a number in a range specified by a max and a min value
 /*function ottieniRandom(max, min){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random()* (max-min)) + min;//il max è escluso e il minimo è incluso
+    return Math.floor(Math.random()* (max-min)) + min;//max value is included and min is not included
 }*/
 
-//scriviamo una funzione che confronta il numero random con il numero inserito nel form
-//richiamiamo la funzione sull'onclick del bottone
-function gioca(){
-        document.getElementById("btnPlay").onclick = function confronta() {
-
-        let num = document.getElementById("inputNumber").value;//acuisisce il valore dalla casella di input del form
-        let numRandom = Math.floor(Math.random() * 6); //genera un valore random tra 0 e 5 metodo più semplice
-        //confronta il valore inserito con il numero random generato
-        if (+num === numRandom)
-            alert("Hai vinto! hai indovinato il numero di dita");
-        else
-            alert("Non hai indovinato, il numero di dita era: " + numRandom);
-        }
+//function that check if the random number is equal to the input number
+//function calling by the click on the submit button using onclick
+document.getElementById("btnPlay").onclick = function confronta() {
+    let num = document.getElementById("inputNumber").value;//acquire the number from the input field
+    let numRandom = Math.floor(Math.random() * 6); //generates a random number from 0 to 5 (simple way to obtain a random number)
+    //check if the random number is equal to the input number
+    if (+num == numRandom)
+        document.getElementById("result").innerHTML = "Hai vinto! hai indovinato il numero di dita";
+    else{
+        document.getElementById("result").innerHTML = "Non hai indovinato, il numero di dita era: " + numRandom;
+        document.getElementById("inputNumber").value = "";
     }
+}
+
         
 
 
